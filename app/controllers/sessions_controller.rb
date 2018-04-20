ccb163d8d6a9a552fb5f5f2b6672df66742e1b8d
James Ferguson <ncjamesferguson@gmail.com>
2018-04-20 17:26:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
 def create
   @user = User.find_or_create_by(uid: auth['uid']) do |u|
     u.name = auth['info']['name']
+    u.email = auth['info']['name']
+    u.image = auth['info']['image']
   end
 
 
